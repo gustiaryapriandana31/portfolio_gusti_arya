f0adf978aba918882a5eec365b4ce9488f274401
gustiaryapriandana31 <100784362+gustiaryapriandana31@users.noreply.github.com>
2023-05-04 06:15:45 +0700

    Update ContactController.php
    change minimal character on field nama

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,7 +14,7 @@ class ContactController extends Controller {
   public function store(Request $request) {
     $validatedContact = $request->validate([
         "email" => "required|email:dns",
-        "nama" => "required|min:10|max:255",
+        "nama" => "required|min:3|max:255",
         "nohp" => "required|numeric",
         "pesan" => "required"
     ],
